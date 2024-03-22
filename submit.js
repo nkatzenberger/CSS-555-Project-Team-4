198b79c6b782763173739d759806437f07246745
nkatzenberger <penguindude1016@gmail.com>
2024-03-21 23:17:16 -0400

    alteration to file upload, still not working tho

---
+++ submit.js
@@ -0,0 +1,27 @@
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
+  form.addEventListener("submit", (e) =>  {
+    e.preventDefault();
+    let postid = uuidv4();
+    let inputElem = document.getElementById("up");
+    let file = inputElem.files[0];
+    // Create new file so we can rename the file
+    let blob = file.slice(0, file.size);
+    newFile = new File([blob], postid); //renames file
+
+    let formData = new FormData();
+    formData.append("up", newFile);
+    fetch("/upload", {
+        method: "POST",
+        body: formData,
+      })
+      .then((res) => res.text())
+      .then(loadPosts());
+  });
+  
