e93386b2b0ea7030f1d456740aa79f6544655d99
Rohan Kallur <r.kallur.21@gmail.com>
2024-03-24 19:05:27 -0400

    converted data to brainbrowser format

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
 # Run this in order to generate the brain images using data
 
-from BaseExample import data_preprocessing, main
+# from BaseExample import data_preprocessing, main
+import scipy.io
+import pandas as pd
 
-if __name__ == "__main__":
-    exec(open("data_preprocessing.py").read())
-    exec(open("main.py").read())
+# if __name__ == "__main__":
+#     exec(open("data_preprocessing.py").read())
+#     exec(open("main.py").read())
+    
+mat = scipy.io.loadmat("./BaseExample/result/sample/Test_result_evoked_LA.mat")
+mat_variable = mat['s_pred']
+df = pd.DataFrame(mat_variable)
+single_col_df = df.stack().to_frame()
+single_col_df = df.to_string(index=False, header=False)
+file = open("converted.txt", "w")
+file.write(str(single_col_df))
+file.close
