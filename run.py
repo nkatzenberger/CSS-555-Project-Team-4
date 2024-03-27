702b70351ff6abbbb9c475353581b0f9d4a7c043
bpenman <bpenman@stevens.edu>
2024-03-27 15:34:24 -0400

    applied deodorant to run.py

--- run.py
+++ run.py
@@ -1,19 +1,25 @@
 """Module for converting data to BrainBrowser format."""
 # Run this in order to generate the brain images using data
 
-# from BaseExample import data_preprocessing, main
 import scipy.io
 import pandas as pd
 
-# if __name__ == "__main__":
-#     exec(open("data_preprocessing.py").read())
-#     exec(open("main.py").read())
 def convertToBB(filename):
-    mat = scipy.io.loadmat("./BaseExample/result/sample/"+filename)
-    mat_variable = mat['s_pred']
-    df = pd.DataFrame(mat_variable)
-    single_col_df = df.stack().to_frame()
-    single_col_df = df.to_string(index=False, header=False)
+    # Construct the full file path
+    file_path = "./BaseExample/result/sample/"+filename
+    
+    # Load data from the file
+    matlab_file = scipy.io.loadmat(file_path)
+    matlab_variable = matlab_file['s_pred']
+    
+    # Convert data to DataFrame
+    df = pd.DataFrame(matlab_variable)
+    
+    # Convert DataFrame to a string
+    single_column_df = df.stack().to_frame()
+    single_column_df = df.to_string(index=False, header=False)
+    
+    # Write the string to a file
     file = open("converted.txt", "w")
-    file.write(str(single_col_df))
-    file.close
+    file.write(str(single_column_df))
+    file.close()
