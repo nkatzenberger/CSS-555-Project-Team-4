dc74756c527ea8d04e18f3c3ac6b0f7835240907
nkatzenberger <penguindude1016@gmail.com>
2024-04-01 18:17:36 -0400

    previous commit forgot to include the function and it's test case

--- test_funcs.py
+++ test_funcs.py
@@ -1,9 +1,14 @@
 import unittest
+import submit
+from firebase_admin import storage
 
-class TestFunctions(unittest.TestCase):
-    def test_upload(self):
-        test = True
-        self.assertTrue(False)
+class testUpload(unittest.TestCase):
+    def test_submit(self):
+        blob1 = submit.uploadToFirebase("evoked_eeg_RV.mat")
+        bucket = storage.bucket()
+        blob2 = bucket.blob("evoked_eeg_RV.mat")
+        blob2.download_to_filename("./down.mat")        
+        self.assertEqual(blob1.name, blob2.name)
 
 if __name__ == "__main__":
     unittest.main()
