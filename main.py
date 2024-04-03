ae36bae5194cdb36cef840ca27befa6b6802633e
Rohan Kallur <r.kallur.21@gmail.com>
2024-04-03 14:08:00 -0400

    removed run.py and integrated necessary function into main.py

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ from flask import Flask, request, render_template, make_response
 from flask_cors import CORS
 import os
 from BaseExample import model
-from run import convertToBB
-#from BaseExample import model
+import scipy.io
+import pandas as pd
+
+# Convert ConvDip data into brainbrowser format
+def convertToBB(filename):
+    # Construct the full file path
+    file_path = "./result/sample/"+filename
+    
+    # Load data from the file
+    matlab_file = scipy.io.loadmat(file_path)
+    matlab_variable = matlab_file['s_pred']
+    
+    # Convert data to DataFrame
+    df = pd.DataFrame(matlab_variable)
+    
+    # Convert DataFrame to a string
+    single_column_df = df.stack().to_frame()
+    single_column_df = df.to_string(index=False, header=False)
+    
+    # Write the string to a file
+    file = open("converted.txt", "w")
+    file.write(str(single_column_df))
+    file.close()
 
 # Get the absolute path to the project root directory
 project_root = os.path.abspath(os.path.dirname(__file__))
