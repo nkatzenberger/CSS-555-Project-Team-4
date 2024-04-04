b29f1f252009f104b2e1cd8ce9e4cdcef9f4ad95
nkatzenberger <penguindude1016@gmail.com>
2024-04-04 19:09:47 -0400

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,9 +1,11 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, credentials } from "firebase/auth";
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 var email = document.getElementById("username");
 var password = document.getElementById("password");
-
+var loginButton = document.getElementById("login-btn");
+cloudkeys = credentials.Certificate("./NewCreds.json");
+loginButton.addEventListener("click", function() {
 signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
@@ -18,10 +20,11 @@ signInWithEmailAndPassword(auth, email, password)
   onAuthStateChanged(auth, (user) => {
     if (user) {
       const uid = user.uid;
-        document.getElementById("current-login") += userCredential.user;
+        document.getElementById("current-login") += credentials.user;
       // ...
     } else {
       // User is signed out
       // ...
     }
   });
+});
