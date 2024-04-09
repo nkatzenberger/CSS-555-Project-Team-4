05be6b0e2b48741eaed683e5e036c40dcb31b1fe
Jack Byrne <132776007+Byrne13@users.noreply.github.com>
2024-04-09 17:19:12 -0400

    Organize Repo Fix 5

--- src/py/test_funcs.py
+++ src/py/test_funcs.py
@@ -7,11 +7,13 @@ class testUpload(unittest.TestCase):
     def test_submit(self):
         base_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
         file_names = ["evoked_eeg_RV.mat", "evoked_eeg_RA.mat", "evoked_eeg_LV.mat", "evoked_eeg_LA.mat"]
-        for file in file_names:
-            blob1 = submit.uploadToFirebase(file)
+        for file_name in file_names:
+            file_path = os.path.join(base_dir, file_name)
+            blob1 = submit.uploadToFirebase(file_path)
             bucket = storage.bucket()
-            blob2 = bucket.blob(file)
-            blob2.download_to_filename("./data/down.mat")        
+            blob2 = bucket.blob(blob1.name)
+            download_path = os.path.join(base_dir, 'down.mat')
+            blob2.download_to_filename(download_path)     
             self.assertEqual(blob1.name, blob2.name)
         
 if __name__ == "__main__":
